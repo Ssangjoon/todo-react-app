dc64be6281941d41491caee2af7bd7195f6041fc
ssang <qkqh34k@gmail.com>
2024-02-16 15:01:13 +0900

    AddTodo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import './App.css';
 import Todo from './Todo';
 import { useState } from 'react';
+import {Container,List, Paper} from "@mui/material"
+import AddTodo from './AddTodo';
 
 function App() {
   const [items, setItem] = useState([
     {
-    id:"0"
-    ,title:"ssang se love da"
-    ,done:true
-  },
-  {
-    id:"1"
-    ,title:"ssang se love da2"
-    ,done:true
-  }
-]);
-  let todoItems = items.length > 0 && items.map((item) => <Todo item={item} key={item.id}/>)
-  console.table(todoItems)
+      id:"0"
+      ,title:"ssang se love da"
+      ,done:true
+    },
+    {
+      id:"1"
+      ,title:"ssang se love da2"
+      ,done:true
+    }
+  ]);
+
+  let todoItems = items.length > 0 && (
+    <Paper style={{margin:16}}>
+      <List>
+        {items.map((item) => (
+          <Todo item={item} key={item.id}/>
+        ))}
+      </List>
+    </Paper>
+  )
+
   return (
     <div className="App">
-      {todoItems} 
+      <Container maxWidth="md">
+        <AddTodo/>
+        <div className='TodoList'>{todoItems}</div>
+      </Container>
     </div>
   );
 }
